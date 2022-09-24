5507f3e3ef4dbd77d0ff6e8477d9c90971076870
sohamdev2 <soham@rivuletdigital.com>
2022-09-25 08:29:58 +1200

    update

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import "./style.css";
 const setupCanvas = (el, classList) => {
   const canvas = document.createElement("canvas")
   canvas.classList = [...classList, 'vtpk-canvas']
-  el.appendChild();
+  el.appendChild(canvas);
 };
 
 export { default as setupCanvas };
