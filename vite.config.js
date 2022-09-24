f1c4ea6cf3ffb9844e068a399ade1ace12359e57
sohamdev2 <soham@rivuletdigital.com>
2022-09-25 08:18:05 +1200

    update

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,16 @@ import { defineConfig } from "vite";
 import path, { resolve } from "path";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, 'src'),
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     "@": resolve(__dirname, 'src'),
+  //   },
+  // },
   build: {
     lib: {
       entry: resolve(__dirname, "main.js"),
       name: "vite-test",
       fileName: 'index',
-      formats: ["es", "cjs", "umd"],
     },
     sourcemap: true,
   },
